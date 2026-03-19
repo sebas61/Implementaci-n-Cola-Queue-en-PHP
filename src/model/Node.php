c84f03060e9d9961038d18878e0423eb84a6a4a8
sebastian quiroga <sebastian03qui@gmail.com>
2026-03-19 18:49:42 -0500

    Implementación Queue en PHP

--- src/model/Node.php
+++ src/model/Node.php
@@ -1,45 +1,34 @@
 <?php
 
-class Node{
+class Node
+{
 
-private $value;
-private $next;
+    private $value;
+    private $next;
 
-public function __construct($value){
-    $this->value = $value;
-    $this->next = null;
+    public function __construct($value)
+    {
+        $this->value = $value;
+        $this->next = null;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    public function getNext()
+    {
+        return $this->next;
+    }
+
+    public function setNext($next)
+    {
+        return $this->next = $next;
+    }
+
+    public function setValue($value)
+    {
+        return $this->value = $value;
+    }
 }
-
-public function getValue(){
-    return $this->value;
-}
-
-public function getNext(){
-    return $this->next;
-}
-
-public function setNext($next){
-    return $this->next = $next;
-}
-
-public function setValue($value){
-    return $this->value = $value;
-}
-
-
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-?>
